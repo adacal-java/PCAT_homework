cca83bbf1634df8eda62d4b92a48f70360cff89e
Eda Cal <edagi09@gmail.com>
2019-12-11 12:45:30 +0000

    PCAT

---
+++ src/test/java/tests/test1.java
@@ -0,0 +1,31 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class test1 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void click() {
+        driver.get("http://practice.cybertekschool.com/");
+    }
+}
---
+++ src/test/java/utilities/WebDriverFactory.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriverFactory {
+
+        public static WebDriver getDriver(String browserType) {
+            WebDriver driver = null;
+
+            switch (browserType.toLowerCase()) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "ie":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new InternetExplorerDriver();
+                    break;
+            }
+
+            return driver;
+        }
+}
