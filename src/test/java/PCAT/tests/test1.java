e69c4a44ad408dfe1cb5587ac571fd30f40350cf
ibrahim BESENK <ibrahimbesenk@gmail.com>
2019-12-13 16:17:34 +0000

    PullAndPush_IbrahimB

--- src/test/java/PCAT/tests/test1.java
+++ src/test/java/PCAT/tests/test1.java
@@ -5,10 +5,18 @@ import PCAT.utilities.ConfigurationReader;
 import org.testng.annotations.Test;
 
 public class test1 extends TestBase{
-
+    /**
+     * I tried to push my code to github.
+     * And i m Ibrahim B.
+     */
 
     @Test
     public void test(){
+
         driver.get(ConfigurationReader.get("url"));
+
+
+
+
     }
 }
